1c1f9bb1f29b6c6b8fb9f3d9ddb31de08d1de331
dazz6 <dazz6@kakao.com>
2025-03-22 17:48:45 +0900

    2.6) 스타일링

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,3 +1,11 @@
+import style from "./index.module.css";
+// app 을 제외한 다른 곳에 global css import 불가능
+
 export default function Home() {
-  return <h1>인덱스</h1>;
+  return (
+    <>
+      <h1 className={style.h1}>인덱스</h1>
+      <h2 className={style.h2}>H2</h2>
+    </>
+  );
 }
