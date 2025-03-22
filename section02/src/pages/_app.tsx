d134871995b90087b681b7229390a65d9c6458c5
dazz6 <dazz6@kakao.com>
2025-03-22 17:33:43 +0900

    2.4) 프리페칭

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -2,6 +2,19 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+// 프리패칭 (pre-fetching)
+// 현재 페이지에서 이동 가능한 모든 경로의 JS를 미리 가져옴 (Link, button...)
+
+// 사전 렌더링 개념에서는 초기 접근이 완료되면 모든 Component 가 로드되어
+// 추가적인 정보를 가져올 필요가 없는데 왜 프리페칭이라는 개념이 존재하느냐?
+// -> Next.js 는 사전 렌더링 과정에서 '모든' JS를 전달하는 것이 아니라 요청 페이지에 대해서만 가져옴
+// -> 사전 렌더링에서 모든 JS 를 가져올 경우 Hydration 및 TTI 지연
+// -> 페이지 이동시에 '다시' 초기 접근의 과정을 거치는 것은 비효율적이므로 프리패칭 기능이 추가로 작동하는 것
+
+// 프로그래메틱한 페이지 이동은 프리패칭이 이루어지지 않음
+// Link을 제외한 페이지 이동도 프리패칭에 포함하고 싶을 경우 useEffect로 prefetch 지정
 
 // React의 App 과 동일
 // 모든 페이지 역할을 하는 Component 들의 root(부모) Component
@@ -16,12 +29,22 @@ export default function App({ Component, pageProps }: AppProps) {
     // router.replace() : 뒤로가기 방지 + 페이지 이동
     // router.back() : 뒤로가기
   };
+
+  useEffect(() => {
+    router.prefetch(`/test`);
+    // 현재 /test 경로는 button을 이용한 프로그래메틱한 페이지 이동임
+    // 프리패칭에 포함되지 않으므로 useRouter의 prefetch를 사용하여 포함
+  }, []);
+
   return (
     <>
       <header>
         <Link href={`/`}>index</Link>&nbsp;
         <Link href={`/search`}>search</Link>&nbsp;
-        <Link href={`/book/1`}>book/1</Link>
+        <Link href={`/book/1`} prefetch={false}>
+          book/1
+          {/* prefetch를 false로 지정하면 Link 를 사용한 경로 이동이라고 하더라도 프리패칭되지 않음 */}
+        </Link>
         {/* HTML 의 a 태그는 이동할 때마다 새로운 HTML 로드
         -> Next.js 에서는 자체 제공하는 Link 이용 */}
         <div>
