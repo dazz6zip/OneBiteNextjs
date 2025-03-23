467cb06b24a493d01b65b9015e9d6136c4e5a7b1
dazz6 <dazz6@kakao.com>
2025-03-23 14:08:10 +0900

    2.9) 한입북스 UI 구현하기

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -2,13 +2,26 @@ import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode } from "react";
 // app 을 제외한 다른 곳에 global css import 불가능
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
+// @ : src 경로 (tsconfig.json 에서 확인 가능)
 
 export default function Home() {
   return (
-    <>
-      <h1 className={style.h1}>인덱스</h1>
-      <h2 className={style.h2}>H2</h2>
-    </>
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>{" "}
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+    </div>
   );
 }
 
