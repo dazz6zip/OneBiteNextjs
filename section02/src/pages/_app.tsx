697e5eaf4ff8173bf45299a4e37ac46a1bb517a0
dazz6 <dazz6@kakao.com>
2025-03-23 13:10:18 +0900

    2.7) 글로벌 레이아웃 설정하기

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,8 +1,6 @@
+import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 // 프리패칭 (pre-fetching)
 // 현재 페이지에서 이동 가능한 모든 경로의 JS를 미리 가져옴 (Link, button...)
@@ -16,43 +14,10 @@ import { useEffect } from "react";
 // 프로그래메틱한 페이지 이동은 프리패칭이 이루어지지 않음
 // Link을 제외한 페이지 이동도 프리패칭에 포함하고 싶을 경우 useEffect로 prefetch 지정
 
-// React의 App 과 동일
-// 모든 페이지 역할을 하는 Component 들의 root(부모) Component
 export default function App({ Component, pageProps }: AppProps) {
-  // Component : 페이지 역할
-  // pageProps : 페이지에 전달될 Props 를 객체 형태로
-
-  const router = useRouter();
-
-  const onClickButton = () => {
-    router.push(`/test`);
-    // router.replace() : 뒤로가기 방지 + 페이지 이동
-    // router.back() : 뒤로가기
-  };
-
-  useEffect(() => {
-    router.prefetch(`/test`);
-    // 현재 /test 경로는 button을 이용한 프로그래메틱한 페이지 이동임
-    // 프리패칭에 포함되지 않으므로 useRouter의 prefetch를 사용하여 포함
-  }, []);
-
   return (
-    <>
-      <header>
-        <Link href={`/`}>index</Link>&nbsp;
-        <Link href={`/search`}>search</Link>&nbsp;
-        <Link href={`/book/1`} prefetch={false}>
-          book/1
-          {/* prefetch를 false로 지정하면 Link 를 사용한 경로 이동이라고 하더라도 프리패칭되지 않음 */}
-        </Link>
-        {/* HTML 의 a 태그는 이동할 때마다 새로운 HTML 로드
-        -> Next.js 에서는 자체 제공하는 Link 이용 */}
-        <div>
-          <button onClick={onClickButton}>/test 페이지로 이동</button>
-          {/* 프로그래메틱한 페이지 이동 (특정 버튼 클릭 혹은 조건 만족시 함수 내에서 페이지 이동) */}
-        </div>
-      </header>
-      <Component {...pageProps} />
-    </>
+    <GlobalLayout>
+      <Component {...pageProps}></Component>
+    </GlobalLayout>
   );
 }
