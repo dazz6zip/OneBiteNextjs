387af158f9410834e4686b28f1b147afcbbe98e0
dazz6 <dazz6@kakao.com>
2025-04-01 11:08:59 +0900

    4.2) 데이터 캐시

--- section04/next.config.ts
+++ section04/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* next 앱의 옵션을 설정해 주는 파일 */
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 export default nextConfig;
--- section04/src/app/(with-searchbar)/page.tsx
+++ section04/src/app/(with-searchbar)/page.tsx
@@ -1,11 +1,23 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
-import books from "@/mock/books.json";
 import { BookData } from "@/types";
+
+// cache 옵션은 fetch 만 가능 (axios 안 됨)
 
 async function AllBooks() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    { cache: "no-store" }
+    /*
+    1. "cache: no-store" : 캐싱 안 함, 접속할 때마다 새로 불러옴
+    2. "cache: force-cache" : 요청 결과 무조건 캐싱 
+      ㄴ 초기 로드시 miss, 로드 후 set, 이후 로드시 hit
+    3. "next : {revalidate : 3}" : 특정 시간(예제의 경우 3초)을 주기로 캐시 업데이트
+      ㄴ 일단 캐싱된 데이터 (stale) 가져온 다음 다시 가져와서 set, 이후 로드시 hit
+    4. "next" : {tags: ['a']} : 요청이 들어왔을 때 데이터 최신화
+
+    * 15 버전부터 fetch 의 기본값 : 캐싱 안 함 (auto no cache)
+    */
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다.</div>;
@@ -23,7 +35,8 @@ async function AllBooks() {
 
 async function RecoBooks() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다.</div>;
