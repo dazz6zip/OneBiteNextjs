8fde7143660ffd2f3abbea50102b5521ac2639d5
dazz6 <dazz6@kakao.com>
2025-04-10 17:58:31 +0900

    5.5) 클라이언트 라우터 캐시

--- section04/src/app/(with-searchbar)/layout.tsx
+++ section04/src/app/(with-searchbar)/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: ReactNode }) {
       <Suspense fallback={<div>Loading...</div>}>
         {/* 클라이언트측에서만 렌더링되도록 Suspense 태그 사용
         searchbar 는 쿼리 스트링을 받는데 사전 렌더링 과정에서는 쿼리 스트링이 없으므로 
-        
+      
         Suspense 로 감싸 줬으므로 쿼리 스트링 로드 전에는 Loading 문구 출력 */}
         <Searchbar />
       </Suspense>
