160288369d91547ec8aa70135dddd7c3439a7c31
dazz6 <dazz6@kakao.com>
2025-04-11 18:50:23 +0900

    6.4) 스켈레톤 UI 적용하기

--- section06/src/app/(with-searchbar)/search/page.tsx
+++ section06/src/app/(with-searchbar)/search/page.tsx
@@ -1,4 +1,5 @@
 import BookItem from "@/components/book-item";
+import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
 import { BookData } from "@/types";
 import { delay } from "@/util/delay";
 import { Suspense } from "react";
@@ -50,7 +51,7 @@ export default async function Page({
       // 검색 페이지 내에서 쿼리스트링만 바뀔 때도 트리거됨
       // useEffect의 의존성 배열처럼 값이 바뀔 때마다 트리거되도록 key 설정
       // react는 Key가 변경되면 새로운 component로 인식하는 것을 활용
-      fallback={<div>Loading...</div>}
+      fallback={<BookListSkeleton count={3} />}
     >
       {/* Suspense -> 스트리밍되도록 자동 설정 */}
       <SearchResult q={(await searchParams).q || ""}></SearchResult>
