2494716bbd93689ab27d895cea2712969fda9ba9
dazz6 <dazz6@kakao.com>
2025-04-27 21:02:56 +0900

    7.6) 클라이언트 컴포넌트에서의 서버액션

--- section07/src/actions/create-review.action.ts
+++ section07/src/actions/create-review.action.ts
@@ -1,11 +1,13 @@
 "use server";
 
+import { delay } from "@/util/delay";
 import { revalidatePath, revalidateTag } from "next/cache";
 
 // 서버 액션으로 설정됨 (브라우저에서 호출 가능한 서버에서만 실행되는 함수)
+// 별도 파일로 분리돼 있지 않을 경우 function 상단에 use server 기재
 
-export async function createReviewAction(formData: FormData) {
-  // 별도 파일로 분리돼 있지 않을 경우 function 상단에 use server 기재
+export async function createReviewAction(_: any, formData: FormData) {
+  // useActionState 를 사용하면 첫 번째 인수 state (사용하지 않을 것이라 _)
 
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
@@ -13,10 +15,14 @@ export async function createReviewAction(formData: FormData) {
   // string 일 경우 ?.toString()
 
   if (!bookId || !content || !author) {
-    return;
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 작성해 주세요.",
+    };
   }
 
   try {
+    await delay(2000);
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
       {
@@ -25,7 +31,10 @@ export async function createReviewAction(formData: FormData) {
         // 객체를 직렬화해서 전달
       }
     );
-    console.log(response.status);
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
 
     // 1. 특정 주소의 해당하는 페이지만 재검증
     // revalidatePath(`/book/${bookId}`);
@@ -42,6 +51,11 @@ export async function createReviewAction(formData: FormData) {
     // 5. 태그 값을 기준으로 데이터 캐시 재검증 (fetch 시 tag 지정 가능)
     revalidateTag(`review-${bookId}`);
 
+    return {
+      status: true,
+      error: ``,
+    };
+
     // revalidatePath 주의사항
     // 1. 서버에서만 호출할 수 있는 메소드
     // 2. 페이지를 전부 재검증하기 때문에 포함된 캐시 무효화됨
@@ -49,7 +63,9 @@ export async function createReviewAction(formData: FormData) {
     // 3. 풀 라우트 캐시까지도 삭제됨
     // 다음 방문시에 다이나믹 페이지처럼 생성됨 (그때 풀 라우트 캐시 업데이트)
   } catch (err) {
-    console.error(err);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다. (${err})`,
+    };
   }
 }
--- section07/src/components/review-editor.tsx
+++ section07/src/components/review-editor.tsx
@@ -1,15 +1,47 @@
+"use client";
+
 import style from "./review-editor.module.css";
 import { createReviewAction } from "@/actions/create-review.action";
+import { useActionState, useEffect } from "react";
+// form 태그 상태관리 hooks
 
 export default function ReviewEditor({ bookId }: { bookId: string }) {
+  const [state, formAction, isPending] = useActionState(
+    // state : 서버 액션 후 돌아온 결과
+    // formAction : 폼 제출시 호출하는 액션 함수
+    // isPending : 액션 실행 중 여부 (로딩 상태)
+
+    createReviewAction, // form 태그 액션
+    null // 초기값
+  );
+
+  useEffect(() => {
+    // 에러 발생시 alert 출력
+    if (state && !state.status) {
+      alert(state.error);
+    }
+  }, [state]);
+
   return (
     <section>
-      <form action={createReviewAction} className={style.form_container}>
+      <form action={formAction} className={style.form_container}>
         <input name="bookId" value={bookId} hidden readOnly />
-        <textarea required name="content" placeholder="리뷰 내용" />
+        <textarea
+          disabled={isPending}
+          required
+          name="content"
+          placeholder="리뷰 내용"
+        />
         <div className={style.submit_container}>
-          <input required name="author" placeholder="작성자" />
-          <button type="submit">작성하기</button>
+          <input
+            disabled={isPending}
+            required
+            name="author"
+            placeholder="작성자"
+          />
+          <button disabled={isPending} type="submit">
+            {isPending ? "..." : "작성하기"}
+          </button>
         </div>
       </form>
     </section>
